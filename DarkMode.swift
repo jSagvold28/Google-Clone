df640602714df2c8afb5f95a717daf87501d7a66
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2022-11-19 07:49:04 -0600

    Related to SettingsView

---
+++ DarkMode.swift
@@ -0,0 +1,35 @@
+//  YourDataInSearch.swift
+//  Google Clone
+//
+//  Created by Jayce Sagvold on 11/19/22.
+//
+
+import SwiftUI
+struct DarkMode: View {
+    @State var showGreeting = true
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Section {
+                    
+                    VStack {
+                        Toggle(isOn: $showGreeting) {
+                            Text("Light theme")
+                        }.padding()
+                        Text("When togled off dark mode on")
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct DarkMode_Previews: PreviewProvider {
+    static var previews: some View {
+        DarkMode()
+    }
+}
+
